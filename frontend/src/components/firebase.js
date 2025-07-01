62a855242ad859e3f28b3cdc78bea0d53fe28246
ElizabethMM36 <194572580+ElizabethMM36@users.noreply.github.com>
2025-07-01 18:14:23 +0400

    firebase

---
+++ frontend/src/components/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMt2C_byPej8eepcRyWZEyDlqAju3WkLU",
+  authDomain: "crumpslogin.firebaseapp.com",
+  projectId: "crumpslogin",
+  storageBucket: "crumpslogin.firebasestorage.app",
+  messagingSenderId: "5230972725",
+  appId: "1:5230972725:web:7af2746eb846533031467c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
