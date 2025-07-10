61425fa3a0f3b94a89cf81cb584e774f7b3854e8
ElizabethMM36 <194572580+ElizabethMM36@users.noreply.github.com>
2025-07-10 14:41:33 +0530

    backend

---
+++ backend/config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY8gWZ99tsp4o2ZBTHOse-mDZsllNIBu4",
+  authDomain: "datacrumps.firebaseapp.com",
+  projectId: "datacrumps",
+  storageBucket: "datacrumps.firebasestorage.app",
+  messagingSenderId: "1013330448426",
+  appId: "1:1013330448426:web:f7460dff70fa8ee936f353"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
